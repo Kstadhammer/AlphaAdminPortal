d56b2ba06bb9995f212fd8e0df6b6be7551500ae
KstadHammer <kim.hammerstad@gmail.com>
2025-03-21 14:38:30 +0100

    Initial commit

---
+++ WebApp/Controllers/AdminController.cs
@@ -0,0 +1,29 @@
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApp.Controllers;
+
+[Authorize]
+public class AdminController : Controller
+{
+    // GET
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult Projects()
+    {
+        return View();
+    }
+
+    public IActionResult Members()
+    {
+        return View();
+    }
+
+    public IActionResult Clients()
+    {
+        return View();
+    }
+}
