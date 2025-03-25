4d1ad679e76d35ee65b18298fd1c69f0fe6a4529
KstadHammer <kim.hammerstad@gmail.com>
2025-03-25 14:13:16 +0100

    Add EditClientForm model and related views

--- WebApp/Controllers/AdminController.cs
+++ WebApp/Controllers/AdminController.cs
@@ -1,4 +1,5 @@
 using Business.Interfaces;
+using Business.Models;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
@@ -20,14 +21,27 @@ public class AdminController(IMemberService memberService) : Controller
         return View();
     }
 
+    [Route("members")]
     public async Task<IActionResult> Members()
     {
         var members = await _memberService.GetAllMembers();
         return View(members);
     }
 
+    [Route("clients")]
     public IActionResult Clients()
     {
+        return View();
+    }
+
+    [HttpPost]
+    public IActionResult AddClient(AddClientForm form)
+    {
+        if (!ModelState.IsValid)
+        {
+            return RedirectToAction("Clients");
+        }
+
         return View();
     }
 }
